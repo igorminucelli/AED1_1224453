ac95376c8803c1a3117111d7631698f337a5d882
igorminucelli <igor.minucelli@unifesp.br>
2026-08-18 14:49:35 -0300

    Implement program to find minimum value and position

--- 1180.c
+++ 1180.c
@@ -1,1 +1,23 @@
-xxd
+#include<stdio.h>
+int main(){
+  
+    int N, menor, posi;
+    
+    scanf("%d",&N);
+    int X[N];
+    scanf("%d",&X[0]);
+    menor = X[0];
+    posi = 0;
+    for(int i = 1; i < N; i++){
+        scanf("%d", &X[i]);
+        if(menor > X[i]){
+            menor = X[i];
+            posi = i;
+        } 
+    }
+    printf("Menor valor: %d\n", menor);
+    printf("Posicao: %d\n", posi);
+    
+    
+    return 0;
+}
