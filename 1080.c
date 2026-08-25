6445d96d7c76167488e2794786eb8f8012280749
igorminucelli <igor.minucelli@unifesp.br>
2026-08-24 21:51:35 -0300

    Update and rename 1180.c to 1080.c

---
+++ 1080.c
@@ -0,0 +1,33 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : <<<Igor Aparecido Minucelli Pinto>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/<<1080>>
+Data        : 24/08/2026
+Objetivo    : Ler um vetor de 100 inteiros e mostrar o maior valor entre eles e a posição.
+Dificuldade : <<<Não houve dificuldade>>>
+Uso de IA   : <<<Se usou, descreva brevemente o uso de IA na solução>>>
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+int main(){
+    int X[100], maior, posi;
+  
+    /*Aqui estou lendo a primeira posição do vetor e assumindo que ela é a maior e dando a posição*/
+    scanf("%d", &X[0]);
+    maior = X[0];
+    posi = 0;
+  
+    /*O laço for inicia em 1, pois a primeira posição já foi lida*/
+    for(int i = 1; i < 100; i++){
+        scanf("%d", &X[i]);
+        if(X[i] > maior){
+            maior = X[i];
+            /*i + 1, pois o vetor começa em 0 e não em 1*/
+            posi = i + 1;
+        }
+    }
+    printf("%d\n", maior);
+    printf("%d\n", posi);
+    
+    return 0;
+}
